af942052a0a5a3305bb8bdbd462f0c36296ea690
Edoardo Tenani <edoardo.tenani@protonmail.com>
2018-07-21 00:51:09 +0200

    add gpg signign after artifacts

--- build.sh
+++ build.sh
@@ -16,9 +16,45 @@ if [ ! $last_tag_ref == $last_commit_ref ]; then
 	exit 1
 fi
 
+mv dist/ dist.prev/
+
 xgo \
     --targets="darwin/amd64,linux/amd64" \
     --dest=dist \
     --ldflags "-X main.version=$version" \
     -v -x \
     github.com/endorama/two-factor-authenticator
+
+gpg_sign_key="edoardo.tenani@protonmail.com"
+checksum_file="dist/two-factor-authenticator_${version}_checksums.txt"
+
+orig_darwin_file="dist/two-factor-authenticator-darwin-10.6-amd64"
+orig_linux_file="dist/two-factor-authenticator-linux-amd64"
+darwin_file="dist/two-factor-authenticator-${version}-darwin-10.6-amd64"
+linux_file="dist/two-factor-authenticator-${version}-linux-amd64"
+
+mv "$orig_darwin_file" "$darwin_file"
+mv "$orig_linux_file" "$linux_file"
+
+gpg2 \
+	-u ${gpg_sign_key} \
+	--output ${darwin_file}.sig \
+	--detach-sign ${darwin_file}
+
+gpg2 \
+	-u ${gpg_sign_key} \
+	--output ${linux_file}.sig \
+	--detach-sign ${linux_file}
+
+sha1sum \
+	"$darwin_file" \
+	"$linux_file" \
+	${checksum_file} \
+	> $checksum_file
+
+gpg2 \
+	-u ${gpg_sign_key} \
+	--output ${checksum_file}.sig \
+	--detach-sign ${checksum_file}
+
+rm -r dist.prev/
